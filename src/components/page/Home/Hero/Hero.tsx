de834abe9dbc93af7ade4c956be163a6a2bbafba
Adahan Jolchuev <147965952+adahanjolchuev@users.noreply.github.com>
2025-05-02 17:31:37 +0600

    footer (#2)
    Co-authored-by: doni21122005 <saparbekovdoni@gmail.com>

--- src/components/page/Home/Hero/Hero.tsx
+++ src/components/page/Home/Hero/Hero.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import scss from "./Hero.module.scss";
 
 const Hero = () => {
   return (
-    <div>
-      Hero
-    </div>
-  )
-}
+    <section className={scss.hero}>
+      <div className={"container"}>
+        <div className={scss.content}>
+          <div className={scss.block_text}>
+            <h1>Back to School</h1>
+            <h3>Welcome to all of our students</h3>
+            <button>Discover</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
